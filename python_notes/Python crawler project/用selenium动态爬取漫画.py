d697b0a942faf56d1878a86b3d4bf53081961e44
lee <1755401052@qq.com>
2025-06-12 12:00:25 +0800

    爬取漫画

--- python_notes/Python crawler project/用selenium动态爬取漫画.py
+++ python_notes/Python crawler project/用selenium动态爬取漫画.py
@@ -5,16 +5,39 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as edge_service #驱动路径
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 import os
+import logging
 
-service= edge_service(executable_path=r"D:\games\edgedriver_win64\msedgedriver.exe")
-options=webdriver.EdgeOptions()
-driver= webdriver.Edge(service=service,options=options)
+logging.basicConfig(level=logging.INFO)
+#创建漫画目录
+base_directory = r'D:\games\computer-learning\爬取的漫画'
+if not os.path.exists(base_directory):
+    os.makedirs(base_directory)
+
+#目标目录
+target_directory=os.path.join(base_directory,'暴走邻家')
+if not os.path.exists(target_directory):
+    os.makedirs(target_directory)
+    print(f"{target_directory}已创建")
+else:
+    print("已存在")
+
+# 配置 Edge 选项
+options = webdriver.EdgeOptions()
+options.add_argument('--ignore-certificate-errors')
+service = edge_service(executable_path=r"D:\games\edgedriver_win64\msedgedriver.exe")
+driver = webdriver.Edge(service=service, options=options)
 
 #确认爬取网站
-targer_url ='https://www.idmzj.com/view/yaoshenji/41917.html'
+targer_url ='https://www.manhuazhan.com/chapter/495475-154500.html'
+
+
+#打开一个网站
+driver.get(targer_url)
 
 #隐形等待10秒
 driver.implicitly_wait(10)
@@ -26,30 +49,19 @@ for i in range(0, scroll_height, 800):
     driver.execute_script(f"window.scrollTo(0, {i});")
     time.sleep(0.5)  # 滚动间隔防封IP
 
-#打开一个网站
-driver.get(targer_url)
+try:
+    img_elements = WebDriverWait(driver, 30).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME,'lazy')))
+    if img_elements:
+        for i ,img in enumerate(img_elements):
+            img_url = img.get_attribute('src')
 
-#定位元素
-img = driver.find_element(By.CLASS_NAME,'scrollbar-demo-item')
-
-#元素操作
-img_url = img.get_attribute('src')
-
-#下载图片
-img_=requests.get(img_url)
-with open("manhua.jpg",'wb') as f:
-    f.write(img_.content)
-
-
-driver.quit()
-
-#解析网站内容
-
-#处理清洗数据
-
-#存储数据
-# 获取页面标题
-
-
-
-
+            # 下载图片
+            img_ = requests.get(img_url)
+            filename = os.path.join(target_directory, f"{i}.jpg")
+            with open(filename, 'wb') as f:
+                f.write(img_.content)
+    else:
+        print("未找到图片元素")
+finally:
+    driver.quit()
